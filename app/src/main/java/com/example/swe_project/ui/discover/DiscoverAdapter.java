4178325f8c04905102d4cc2e2b8a8d729fc59ee3
shreyansh <shreyansh3926@gmail.com>
2020-08-24 17:28:35 +0530

    Created Discover page UI

---
+++ app/src/main/java/com/example/swe_project/ui/discover/DiscoverAdapter.java
@@ -0,0 +1,64 @@
+package com.example.swe_project.ui.discover;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.swe_project.R;
+
+import java.util.List;
+
+public class DiscoverAdapter extends RecyclerView.Adapter<DiscoverAdapter.DiscoverViewHolder> {
+
+    public static class DiscoverViewHolder extends RecyclerView.ViewHolder{
+        TextView title,dist, name, phone, desc;
+
+        DiscoverViewHolder(View itemView){
+            super(itemView);
+            title=itemView.findViewById(R.id.titletext);
+            dist=itemView.findViewById(R.id.distance);
+            name=itemView.findViewById(R.id.name);
+            phone=itemView.findViewById(R.id.phone);
+            desc=itemView.findViewById(R.id.description);
+        }
+    }
+
+    List<DiscoverData> data;
+    DiscoverAdapter(List<DiscoverData> datalist){
+        this.data=datalist;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+
+
+    @Override
+    public DiscoverViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.custom_view_discover, viewGroup, false);
+        DiscoverViewHolder pvh = new DiscoverViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(DiscoverViewHolder discoverViewHolder, int i) {
+        discoverViewHolder.title.setText(data.get(i).title);
+        discoverViewHolder.dist.setText(data.get(i).distance);
+        discoverViewHolder.name.setText(data.get(i).name);
+        discoverViewHolder.phone.setText(data.get(i).phone);
+        discoverViewHolder.desc.setText(data.get(i).description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+}
--- app/src/main/java/com/example/swe_project/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/example/swe_project/ui/discover/DiscoverFragment.java
@@ -11,14 +11,44 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.swe_project.R;
 
+import java.util.ArrayList;
+
 public class DiscoverFragment extends Fragment {
+
+    RecyclerView recyclerview;
+    DiscoverAdapter adapter;
+    ArrayList<DiscoverData> items;//in this arraylist data is to be loaded from server to be shown in cards
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_discover, container, false);
+        initialiseitems();
+        recyclerview = root.findViewById(R.id.recyclerViewdiscover);
+        recyclerview.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        adapter = new DiscoverAdapter(items);
+        recyclerview.setAdapter(adapter);
         return root;
+    }
+
+    private void initialiseitems() {
+        items = new ArrayList<>();
+        //here you will have to pull data from server and insert it based on distance from donor in this arraylist
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+        items.add(new DiscoverData("NGO name", "23 km", "bunny bhai", "9154862562", "have been feeding people since '98. Aim that no one sleeps hungry"));
+
     }
 }
