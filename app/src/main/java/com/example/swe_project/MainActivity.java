e95f21e97a83c14fc391a10113040c78df91c56f
krish7777 <krishnendusudheesh4@gmail.com>
2020-08-26 17:34:09 +0530

    token validation through backend server

--- app/src/main/java/com/example/swe_project/MainActivity.java
+++ app/src/main/java/com/example/swe_project/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.swe_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 
 import android.widget.EditText;
+import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -27,6 +31,44 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MainActivity", "testing git here");
        // getActionBar().hide();
         getSupportActionBar().hide();
+
+        if(AuthChecker.authChecker(this)==true){
+
+            String url = "http://192.168.1.4:8000/auth/isAuth";
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        Log.d("Response here", response.toString());
+                        Intent intent = new Intent(MainActivity.this,UserActivity.class);
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.d("Response", "error");
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Response error", String.valueOf(error));
+                    AuthChecker.logout(MainActivity.this);
+                }
+            }){
+                @Override
+                public Map<String, String> getHeaders()
+                {
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    String token = sharedPreferences.getString("token","");
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("Authorization",token);
+                    return headers;
+                }
+            };
+
+            VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        }
+
     }
 
     @Override
