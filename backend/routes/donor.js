a27f36c85e4c23cdc4e2198041950950e36b44fd
arpit1912 <arpitbandejiya@gmail.com>
2020-09-02 19:40:00 +0530

    added donation feature in backend-routes

--- backend/routes/donor.js
+++ backend/routes/donor.js
@@ -10,6 +10,8 @@ router.post('/upload-profile-pic', isAuth, uploadDonorProfilePic)
 router.get('/get-details', isAuth, getDonor)
 
 const OrganisationModel = require('../models/organisation')
+const Donationmodel = require('../models/donation')
+const Donormodel = require('../models/donor')
 
 router.post('/nearbyorganisations', (req, res) => {
 	console.log("lets give the nearby organisation")
@@ -27,5 +29,34 @@ router.post('/nearbyorganisations', (req, res) => {
 	}).limit(2)
 
 })
+router.post('/makedonation',async (req,res)=>{
 
+	console.log(" this is making donation now")
+	const {id,description,latitude,longitude} = req.body
+
+	Donormodel.findById(id).then((donor)=>{
+		if(!donor)
+			throw new Error()
+			var donation = new Donationmodel({
+					donor: donor._id,
+					description:description,
+					latitude:latitude,
+					longitude:longitude
+
+			})
+
+			donation.save((err,donation)=>{
+				if(err){
+					req.flash("error",err.message)
+				}
+				else {
+					res.status(400).json(donation)
+				}
+			})
+
+	}).catch((e)=>{
+		console.log("not working!!!")
+	})
+
+})
 module.exports = router
