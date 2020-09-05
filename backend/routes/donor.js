f5434ae18f2115f253ca7ee11f046bbc0f79eac4
arpit1912 <arpitbandejiya@gmail.com>
2020-09-05 15:50:02 +0530

    merged the other features

--- backend/routes/donor.js
+++ backend/routes/donor.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const isAuth = require('../middlewares/isAuth');
 
-const { getDonor, uploadDonorProfilePic } = require('../controllers/donor');
+const { getDonor, uploadDonorProfilePic, getNearbyOrganisations, getLeaderboard, makeDonation } = require('../controllers/donor');
 
 
 router.post('/upload-profile-pic', isAuth, uploadDonorProfilePic)
@@ -12,20 +12,12 @@ const OrganisationModel = require('../models/organisation')
 const Donationmodel = require('../models/donation')
 const Donormodel = require('../models/donor')
 
-router.post('/nearbyorganisations', (req, res) => {
-	console.log("lets give the nearby organisation")
-	OrganisationModel.find({}, (err, organisation) => {
-		if (err) {
-			console.log("error!!!!!")
-		}
-		else {
-			console.log(organisation)
-			res.status(200).json({ "organisations": organisation })
-		}
+router.post('/nearbyorganisations', isAuth, getNearbyOrganisations)
 
-	}).limit(15)
+router.get('/leaderboard', getLeaderboard)
 
-})
+
+router.post('/makedonation', isAuth, makeDonation)
 
 router.post('/update',async (req,res)=>{
 	console.log("this is the update ")
@@ -56,39 +48,4 @@ router.post('/update',async (req,res)=>{
 
 })
 
-
-router.post('/makedonation',async (req,res)=>{
-
-	console.log(" this is making donation now")
-	const {id,description,latitude,longitude} = req.body
-	console.log(id)
-	console.log(description)
-	console.log(latitude)
-	console.log(longitude)
-
-	Donormodel.findById(id).then((donor)=>{
-		if(!donor)
-			throw new Error()
-			var donation = new Donationmodel({
-					donor: donor._id,
-					description:description,
-					latitude:latitude,
-					longitude:longitude
-
-			})
-
-			donation.save((err,donation)=>{
-				if(err){
-					req.flash("error",err.message)
-				}
-				else {
-					res.status(400).json(donation)
-				}
-			})
-
-	}).catch((e)=>{
-		console.log("not working!!!")
-	})
-
-})
 module.exports = router
